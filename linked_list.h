4548e06ecb035fee7d437ae1ff2272c17f80e452
Shev99 <31804390+Shev99@users.noreply.github.com>
2017-10-17 23:51:34 -0400

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -14,7 +14,7 @@ void print_list(song_node * begin);
 
 song_node * find_artist(char* artist);
 
-song_node * find_song(char* artist);
+song_node * find_song(song_node * begin);
 
 song_node * random_node(song_node * begin);//why would you need any parameters here?
 
