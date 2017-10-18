fb207487f938c1863f132d64e9cde4e3a2475b3b
Alex Shevchenko <alexshevchenko9999@gmail.com>
2017-10-18 13:17:51 -0400

    Did a little here

---
+++ playlist.c
@@ -0,0 +1,30 @@
+void add_song(char *song){
+  
+}
+
+
+void print_songs(char *artist){
+  int i = artist[0] - 'a';
+  struct song_node * begin = table[i];
+  while (begin){
+    if (strcmp(begin->artist,artist)==0) printf("%s\n", begin->name);
+  }
+}
+
+
+void print_letter(char *letter){
+  int i = letter - 'a';
+  struct song_node * begin = table[i];
+  while (begin){
+    printf("Artist: %s , Song: %s\n", begin->artist, begin->song);
+    begin = negin->next;
+  }
+}
+
+
+void print_all(){
+  int i = 0;
+  for (;i <26;i++){
+    print_list(table[i]);
+  }
+}
---
+++ playlist.h
@@ -0,0 +1,15 @@
+void add_song(char *song, char *artist);
+
+struct song_node * find_song(char *song);//you do this
+
+struct song_node * find_artist(char *artist);//you do this
+
+void print_songs(char *artist);
+
+void print_letter(char);
+
+void print_all();
+
+void remove_song(char *song);
+
+void remove_all();
